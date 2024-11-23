7e09c0e99f9688790f9f05d7e83e9a22140c37e3
limcpf <daeseong0226@gmail.com>
2024-11-24 00:12:20 +0900

    feat: SEO 개선

--- src/routes/post/$title/index.route.tsx
+++ src/routes/post/$title/index.route.tsx
@@ -1,8 +1,10 @@
+import { Helmet } from "react-helmet";
 import { readPostApi, readPostKey } from "@/lib/api/post-api";
 import useMenuStore, { MenuEnum } from "@/lib/store/menu.store";
 import { queryOptions } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { lazy } from "react";
+import { convertUnderbar } from "sssh-library";
 
 const PostDetail = lazy(
 	() => import("@/components/custom-ui/post/detail/post-detail"),
@@ -40,6 +42,47 @@ export const Route = createFileRoute("/post/$title/")({
 
 		if (!data) return <PostDetailEmpty />;
 
-		return <PostDetail post={data} />;
+		return (
+			<>
+				<Helmet>
+					<title>{convertUnderbar(data.title, true)} - 싱싱상회</title>
+					{data.series ? (
+						<meta
+							name="description"
+							content={`${data.topic.name}-${data.series.name}시리즈로 작성된 게시글입니다.`}
+						/>
+					) : (
+						<meta
+							name="description"
+							content={`${data.topic.name}주제로 작성된 게시글입니다.`}
+						/>
+					)}
+					<meta
+						name="keywords"
+						content={data.title
+							.replace(/[^가-힣a-zA-Z0-9]/g, "-")
+							.split("-")
+							.filter((t) => t)
+							.join(",")}
+					/>
+					<meta property="og:title" content={data.title} />
+					{data.series ? (
+						<meta
+							name="og:description"
+							content={`${data.topic.name}-${data.series.name}시리즈로 작성된 게시글입니다.`}
+						/>
+					) : (
+						<meta
+							name="og:description"
+							content={`${data.topic.name}주제로 작성된 게시글입니다.`}
+						/>
+					)}
+					{data.thumbnail && (
+						<meta property="og:image" content={data.thumbnail} />
+					)}
+				</Helmet>
+				<PostDetail post={data} />;
+			</>
+		);
 	},
 });
