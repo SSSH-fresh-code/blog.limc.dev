1e9a74f9eb4394b6a410d41d2fe49bdbca33c699
limcpf <daeseong0226@gmail.com>
2024-11-28 12:51:25 +0900

    feat: code highlighting 추가2

--- src/components/custom-ui/post/detail/post-detail-content.tsx
+++ src/components/custom-ui/post/detail/post-detail-content.tsx
@@ -1,8 +1,11 @@
 import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 
 function PostDetailContent({ content }: { content: string }) {
 	return (
 		<Markdown
+			rehypePlugins={[rehypeHighlight]}
 			components={{
 				img: ({ node, ...props }) => (
 					<>
@@ -12,7 +15,14 @@ function PostDetailContent({ content }: { content: string }) {
 							loading="lazy"
 							style={{ margin: "0 auto", objectFit: "scale-down" }}
 						/>
-						<div style={{ textAlign: "center", color: "gray", fontSize: "0.9em", paddingBottom: "10px" }}>
+						<div
+							style={{
+								textAlign: "center",
+								color: "gray",
+								fontSize: "0.9em",
+								paddingBottom: "10px",
+							}}
+						>
 							{props.alt}
 						</div>
 					</>
