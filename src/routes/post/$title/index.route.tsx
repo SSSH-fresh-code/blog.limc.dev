a677b638b59e6d67f3c4de8d627e2ec9578e849b
limcpf <daeseong0226@gmail.com>
2024-11-28 12:50:38 +0900

    fix: lint, format

--- src/routes/post/$title/index.route.tsx
+++ src/routes/post/$title/index.route.tsx
@@ -1,9 +1,9 @@
-import { Helmet } from "react-helmet";
 import { readPostApi, readPostKey } from "@/lib/api/post-api";
 import useMenuStore, { MenuEnum } from "@/lib/store/menu.store";
 import { queryOptions } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { lazy } from "react";
+import { Helmet } from "react-helmet";
 import { convertUnderbar } from "sssh-library";
 
 const PostDetail = lazy(
